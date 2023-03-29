1dd6d0539fc10051aaac8fa40b7fda44b51daf90
Selva81294 <selva81294@gmail.com>
2023-03-29 18:24:03 +0530

    commit

--- routers/ForgotPassword.js
+++ routers/ForgotPassword.js
@@ -23,7 +23,7 @@ router.post("/", async (req, res) => {
       expiresIn: "30m",
     });
 
-    const link = `https://stack-overflow-backend-kohl.vercel.app/api/resetpassword/${oldUser._id}/${token}`;
+    // const link = `https://stack-overflow-backend-kohl.vercel.app/api/resetpassword/${oldUser._id}/${token}`;
     
     var transporter = nodemailer.createTransport({
       service: "gmail",
@@ -37,7 +37,7 @@ router.post("/", async (req, res) => {
       from: "selva81294@gmail.com",
       to: "kumaranselva888@gmail.com",
       subject: "Password Reset",
-      html: link,
+      html: '<p>Click <a href=`https://stack-overflow-backend-kohl.vercel.app/api/resetpassword/${oldUser._id}/${token}`>here</a> to reset your password</p>'
     };
 
     transporter.sendMail(mailOptions, function (error, info) {
