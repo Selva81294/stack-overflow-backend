430c471d952682d1e47bdfb9e5ef5e05d8da2c03
Selva81294 <selva81294@gmail.com>
2023-03-29 16:59:02 +0530

    commit

--- routers/ForgotPassword.js
+++ routers/ForgotPassword.js
@@ -23,7 +23,7 @@ router.post("/", async (req, res) => {
       expiresIn: "30m",
     });
 
-    const link = `https://stack-overflow-backend-clone.vercel.app/api/resetpassword/${oldUser._id}/${token}`;
+    const link = `https://stack-overflow-backend-kohl.vercel.app/api/resetpassword/${oldUser._id}/${token}`;
     
 
     var transporter = nodemailer.createTransport({
