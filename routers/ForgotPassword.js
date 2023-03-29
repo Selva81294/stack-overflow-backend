224d5dfd4322c309d5369872ff14b29e19e11b3d
Selva81294 <selva81294@gmail.com>
2023-03-29 18:28:06 +0530

    commit

--- routers/ForgotPassword.js
+++ routers/ForgotPassword.js
@@ -27,6 +27,7 @@ router.post("/", async (req, res) => {
     
     var transporter = nodemailer.createTransport({
       service: "gmail",
+      secure:false,
       auth: {
         user: "selva81294@gmail.com",
         pass: "yeiomrnsgednzgnx",
