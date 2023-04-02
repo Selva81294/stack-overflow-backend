885683a2f235e4b3cefca8bfec6700ae8e09df18
Selva81294 <selva81294@gmail.com>
2023-04-02 17:56:55 +0530

    commit

--- db.js
+++ db.js
@@ -1,12 +1,13 @@
 const mongoose = require("mongoose")
 
 module.exports.connect= () =>{
+    const URL = process.env.MONGO_URL.toString()
     const params = {
        useNewUrlParser : true,
        useUnifiedTopology : true,
     };
     try {
-        mongoose.connect(process.env.MONGO_URL, params)
+        mongoose.connect(URL, params)
         console.log("Mongo DB is Connected")
     } catch (error) {
         console.log("Mongodb connection error", error)
